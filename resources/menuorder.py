846267cefc3a6c8030949ca1e5319dd3db75b23c
akshaygupta1996 <gupta.akshay1996@gmail.com>
2017-08-02 03:32:49 +0530

    7.8

--- resources/menuorder.py
+++ resources/menuorder.py
@@ -142,7 +142,7 @@ class MenuOrderResource(Resource):
 
 				user = UsersModel.find_by_id(data['user_id'])
 				address = UsersAddressModel.find_by_id(data['address_id'])
-				data = {"user_id": data['user_id'], "status": "0", "order": order.json(), "payment": payment.json(), "menu": menu, "user": user.json(), "address": address.json(), "datetime": datetime.datetime.now()}
+				data = {"user_id": data['user_id'], "status": "0", "order": order.json(), "payment": payment.json(), "menu": menu, "user": user.json(), "address": address.json(), "datetime": str(datetime.datetime.now())}
 				dbfirebase.child("orders").child(order_no).push(data)
 				admin = AdminModel.find_by_username("admin")
 				print admin.fcmtoken
